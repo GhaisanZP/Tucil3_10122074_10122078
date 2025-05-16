f005e437e4ee57e974eb78babaa7c9411c7cb13d
Muhammad Zakkiy <muhammadzakkiy6@gmail.com>
2025-05-16 10:39:43 +0700

    add heuristic

--- src/util/Heuristics.java
+++ src/util/Heuristics.java
@@ -4,13 +4,6 @@ import model.Position;
 
 public class Heuristics {
     public static int heuristic(char[][] board, Position exit, int rows, int cols) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (board[i][j] == 'P') {
-                    return Math.abs(i - exit.row) + Math.abs(j - exit.col);
-                }
-            }
-        }
-        return Integer.MAX_VALUE;
+        return 0
     }
 }
