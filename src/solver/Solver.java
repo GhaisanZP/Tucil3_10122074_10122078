f8e87b06fd2665a64d41beb2c7bf57c5c839274b
Muhammad Zakkiy <muhammadzakkiy6@gmail.com>
2025-05-20 17:12:41 +0700

    remove unused function

--- src/solver/Solver.java
+++ src/solver/Solver.java
@@ -21,17 +21,6 @@ public class Solver {
     private static long nodesExpanded;
     private static long startTime, endTime;
 
-    public static int heuristic(char[][] board, Position exit) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 'P') {
-                    return Math.abs(i - exit.row) + Math.abs(j - exit.col);
-                }
-            }
-        }
-        return Integer.MAX_VALUE;
-    }
-
     public static void solve(char[][] initialBoard, int n_rows, int n_cols, Position exit) {
         rows = n_rows;
         cols = n_cols;
